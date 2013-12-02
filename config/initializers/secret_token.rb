afcd81eadaba80416d6f7ecf86ccd5ab7fe31168
Will Patrick <wgpatrick@gmail.com>
2013-12-02 16:48:47 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Momwebsite::Application.config.secret_token = '9a21fedff4da7ea81454728760be867b768d92a94208beed36f18ec4a1091c7bfc2a8beec88e3f4704a8a05c981e2b0a8b854d3d9ca2cd7653452ebba32347bb'
